0744fae8c701bb0ba976208aaaaba4898bc98d5e
Fabian Braun <fsbraun@gmx.de>
2022-01-18 18:26:20 +0100

    Fix:		Refactor mixins to live in frmeworks modules

---
+++ djangocms_frontend/contrib/collapse/frameworks/bootstrap5.py
@@ -0,0 +1,4 @@
+class CollapseContainerRenderMixin:
+    def render(self, context, instance, placeholder):
+        context["add_classes"] = "collapse"
+        return super().render(context, instance, placeholder)
--- djangocms_frontend/contrib/collapse/renderer/bootstrap5.py
+++
@@ -1,4 +0,0 @@
-class CollapseContainerRenderMixin:
-    def render(self, context, instance, placeholder):
-         context["add_classes"] = "collapse"
-         return super().render(context, instance, placeholder)
