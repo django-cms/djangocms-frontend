2cf999127121e2a21aee9ba8406cebbee5821f76
Fabian Braun <fsbraun@gmx.de>
2022-01-24 21:11:10 +0100

    Fix:		List group item links formatting

--- djangocms_frontend/contrib/link/frameworks/bootstrap5.py
+++ djangocms_frontend/contrib/link/frameworks/bootstrap5.py
@@ -1,23 +1,28 @@
 class LinkRenderMixin:
     def render(self, context, instance, placeholder):
         link_classes = []
+        parent = context.get("parent", None)
+        if parent and parent.ui_item == "ListGroup":
+            link_classes.append("list-group-item")
+            link_classes.append("list-group-item-action")
+            background_prefix = "list-group-item"
+        else:
+            background_prefix = "btn"
         if instance.link_context:
             if instance.link_type == "link":
                 link_classes.append("text-{}".format(instance.link_context))
             else:
                 link_classes.append("btn")
                 if not instance.link_outline:
-                    link_classes.append("btn-{}".format(instance.link_context))
+                    link_classes.append(
+                        "{}-{}".format(background_prefix, instance.link_context)
+                    )
                 else:
                     link_classes.append("btn-outline-{}".format(instance.link_context))
         if instance.link_size:
             link_classes.append(instance.link_size)
         if instance.link_block:
             link_classes.append("btn-block")
-        parent = context.get("parent", None)
-        if parent and parent.ui_item == "ListGroup":
-            link_classes.append("list-group-item")
-            link_classes.append("list-group-item-action")
 
         context["link"] = instance.get_link()
         context["link_classes"] = " ".join(link_classes)
