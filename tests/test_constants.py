52a3f3239888e4c95e76ba965237bb4cc6783995
Fabian Braun <fsbraun@gmx.de>
2022-01-25 10:40:49 +0100

    Fix:		test for constants Add:		Migration for forms

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 
 from djangocms_frontend.settings import (
-    ALIGN_CHOICES, COLOR_STYLE_CHOICES, DEVICE_CHOICES, DEVICE_SIZES,
+    ALIGN_CHOICES,
+    COLOR_STYLE_CHOICES,
+    DEVICE_CHOICES,
+    DEVICE_SIZES,
     TAG_CHOICES,
 )
 
@@ -11,34 +14,46 @@ class B5ConstantsTestCase(TestCase):
     # when changing any values in these
 
     def test_constants(self):
-        self.assertEquals(DEVICE_CHOICES, (
-            ('xs', 'Extra small'),   # default <576px
-            ('sm', 'Small'),         # default ≥576px
-            ('md', 'Medium'),        # default ≥768px
-            ('lg', 'Large'),         # default ≥992px
-            ('xl', 'Extra large'),   # default ≥1200px
-        ))
-        self.assertEquals(DEVICE_SIZES, ('xs', 'sm', 'md', 'lg', 'xl'))
-        self.assertEquals(TAG_CHOICES, (
-            ('div', 'div'),
-            ('section', 'section'),
-            ('article', 'article'),
-            ('header', 'header'),
-            ('footer', 'footer'),
-            ('aside', 'aside')
-        ))
-        self.assertEquals(COLOR_STYLE_CHOICES, (
-            ('primary', 'Primary'),
-            ('secondary', 'Secondary'),
-            ('success', 'Success'),
-            ('danger', 'Danger'),
-            ('warning', 'Warning'),
-            ('info', 'Info'),
-            ('light', 'Light'),
-            ('dark', 'Dark'),
-        ))
-        self.assertEquals(ALIGN_CHOICES, (
-            ('text-start', 'Left'),
-            ('text-center', 'Center'),
-            ('text-end', 'Right'),
-        ))
+        self.assertEquals(
+            DEVICE_CHOICES,
+            (
+                ("xs", "Extra small"),  # default <576px
+                ("sm", "Small"),  # default ≥576px
+                ("md", "Medium"),  # default ≥768px
+                ("lg", "Large"),  # default ≥992px
+                ("xl", "Extra large"),  # default ≥1200px
+            ),
+        )
+        self.assertEquals(DEVICE_SIZES, ("xs", "sm", "md", "lg", "xl"))
+        self.assertEquals(
+            TAG_CHOICES,
+            (
+                ("div", "div"),
+                ("section", "section"),
+                ("article", "article"),
+                ("header", "header"),
+                ("footer", "footer"),
+                ("aside", "aside"),
+            ),
+        )
+        self.assertEquals(
+            COLOR_STYLE_CHOICES,
+            (
+                ("primary", "Primary"),
+                ("secondary", "Secondary"),
+                ("success", "Success"),
+                ("danger", "Danger"),
+                ("warning", "Warning"),
+                ("info", "Info"),
+                ("light", "Light"),
+                ("dark", "Dark"),
+            ),
+        )
+        self.assertEquals(
+            ALIGN_CHOICES,
+            (
+                ("start", "Left"),
+                ("center", "Center"),
+                ("end", "Right"),
+            ),
+        )
