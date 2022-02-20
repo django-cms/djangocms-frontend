a7c068daa7a64b75e0a4b74bad014770aa74b946
Fabian Braun <fsbraun@gmx.de>
2022-02-21 00:14:28 +0100

    add:		.readthedocs.yaml

--- aldryn_config.py
+++ aldryn_config.py
@@ -4,19 +4,18 @@ from aldryn_client import forms
 class Form(forms.BaseForm):
 
     grid_size = forms.NumberField(
-        'Maximum columns to support, default is 12.',
-        required=False
+        "Maximum columns to support, default is 12.", required=False
     )
     enable_icons = forms.CheckboxField(
-        'Enable icon support',
+        "Enable icon support",
         required=False,
         initial=True,
     )
 
     def to_settings(self, data, settings):
-        if data['grid_size']:
-            settings['DJANGOCMS_BOOTSTRAP5_GRID_SIZE'] = int(data['grid_size'])
-        if data['enable_icons']:
-            settings['DJANGOCMS_BOOTSTRAP5_USE_ICONS'] = int(data['enable_icons'])
+        if data["grid_size"]:
+            settings["DJANGOCMS_FRONTEND_GRID_SIZE"] = int(data["grid_size"])
+        if data["enable_icons"]:
+            settings["DJANGOCMS_FRONTEND_USE_ICONS"] = int(data["enable_icons"])
 
         return settings
