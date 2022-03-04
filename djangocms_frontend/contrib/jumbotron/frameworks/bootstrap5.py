e70b81067266017e03f7ed7a0436f290f85d78f4
Mark Walker <mark.walker@realbuzz.com>
2022-03-04 16:12:49 +0000

    Removing unused imports

--- djangocms_frontend/contrib/jumbotron/frameworks/bootstrap5.py
+++ djangocms_frontend/contrib/jumbotron/frameworks/bootstrap5.py
@@ -1,12 +1,3 @@
-from django import forms
-from django.utils.translation import gettext_lazy as _
-from entangled.forms import EntangledModelFormMixin
-
-from djangocms_frontend import settings
-from djangocms_frontend.fields import ButtonGroup
-from djangocms_frontend.helpers import insert_fields
-
-
 class JumbotronRenderMixin:
     def render(self, context, instance, placeholder):
         if not getattr(instance, "background_context", False):
