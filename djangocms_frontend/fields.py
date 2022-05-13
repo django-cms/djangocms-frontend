f4adaedbe223e5b8863914c95b1a23ef4f2f562c
Fabian Braun <fsbraun@gmx.de>
2022-05-13 22:35:51 +0200

    Fix:		Show template form field if template selection is not valid (or otherwise form cannot be closed)

--- djangocms_frontend/fields.py
+++ djangocms_frontend/fields.py
@@ -9,6 +9,18 @@ from . import settings
 from .helpers import first_choice
 
 
+class TemplateChoiceMixin:
+    """Mixin that hides the template field if only one template is available and is selected"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if "template" in self.fields:
+            template_field = self.fields["template"]
+            choices = template_field.choices
+            object_data = kwargs.get("instance").config
+            if len(choices) == 1 and object_data.get("template", "") == choices[0][0]:
+                template_field.widget = forms.HiddenInput
+
+
 class ButtonGroup(forms.RadioSelect):
     template_name = "djangocms_frontend/admin/widgets/button_group.html"
     option_template_name = "djangocms_frontend/admin/widgets/button_group_option.html"
