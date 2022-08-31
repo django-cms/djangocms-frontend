f71f53ab2e3f8c1b11b86bd24e28692b5619bdc4
Fabian Braun <fsbraun@gmx.de>
2022-08-31 10:35:07 +0200

    Remove:		forms app

--- djangocms_frontend/contrib/frontend_forms/helper.py
+++
@@ -1,8 +0,0 @@
-from djangocms_frontend import settings
-
-global_options = settings.FORM_OPTIONS
-
-
-def get_option(form, option, default=None):
-    form_options = getattr(getattr(form, "Meta", None), "options", {})
-    return form_options.get(option, global_options.get(option, default))
