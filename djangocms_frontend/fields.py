bb23c52e18733ab88855a96140466e8128543897
Fabian Braun <fsbraun@gmx.de>
2022-09-22 17:14:21 +0200

    Fix:		removed unused import

--- djangocms_frontend/fields.py
+++ djangocms_frontend/fields.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.forms import TextInput
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 from djangocms_attributes_field import fields
