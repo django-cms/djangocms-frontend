aa37328ceb02c8629a31a1dd7b14a718c3053617
Fabian Braun <fsbraun@gmx.de>
2023-06-02 00:58:32 +0200

    Update tests

--- tests/link/test_urlconf.py
+++ tests/link/test_urlconf.py
@@ -1,11 +1,14 @@
+from unittest import skipIf
+
 from cms.test_utils.testcases import CMSTestCase
 from cms.utils.apphook_reload import reload_urlconf
 from django.urls import NoReverseMatch, reverse
 
 from djangocms_frontend.contrib.link.forms import LinkForm
-from tests.fixtures import TestFixture
+from tests.fixtures import DJANGO_CMS4, TestFixture
 
 
+@skipIf(DJANGO_CMS4, "Tests for django CMS 4 use URL manager with own select2")
 class TestUrlConfTestCase(TestFixture, CMSTestCase):
     def test_select2_url_reversible(self):
         try:
