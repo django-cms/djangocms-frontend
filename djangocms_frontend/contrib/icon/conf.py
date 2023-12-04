68e9776d08aa721198aa6791fd0db08666c557bd
Fabian Braun <fsbraun@gmx.de>
2023-12-04 10:59:17 +0100

    Add icon vendor licences, remove icomoon and happy icons due to unclear licence

--- djangocms_frontend/contrib/icon/conf.py
+++ djangocms_frontend/contrib/icon/conf.py
@@ -26,8 +26,6 @@ ICON_LIBRARIES_SHOWN = getattr(settings, "DJANGOCMS_FRONTEND_ICONS_LIBRARIES_SHO
     "foundation-icons",
     "elegant-icons",
     "feather-icons",
-    "happy-icons",
-    "icomoon",
     "open-iconic",
     "tabler-icons",
     "zondicons",
