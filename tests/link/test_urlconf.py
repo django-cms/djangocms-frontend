d1392f37f22d568818ea753ac3a05f6e6cedae27
Fabian Braun <fsbraun@gmx.de>
2024-04-25 00:45:21 +0200

    fix tests

--- tests/link/test_urlconf.py
+++ tests/link/test_urlconf.py
@@ -1,10 +1,9 @@
 from unittest import skipIf
 
 from cms.test_utils.testcases import CMSTestCase
-from cms.utils.apphook_reload import reload_urlconf
-from django.urls import NoReverseMatch, reverse
+from cms.utils.urlutils import admin_reverse
+from django.urls import NoReverseMatch
 
-from djangocms_frontend.contrib.link.forms import LinkForm
 from tests.fixtures import DJANGO_CMS4, TestFixture
 
 
@@ -12,24 +11,6 @@ from tests.fixtures import DJANGO_CMS4, TestFixture
 class TestUrlConfTestCase(TestFixture, CMSTestCase):
     def test_select2_url_reversible(self):
         try:
-            reverse("dcf_autocomplete:ac_view")
-        except NoReverseMatch as e:
-            self.fail(str(e))
-
-    def test_select2_url_robust_after_url_reload(self):
-        # reload url but do not execute AppConfig.ready() where
-        reload_urlconf()
-
-        # Check url is gone
-        with self.assertRaises(NoReverseMatch):
-            reverse("dcf_autocomplete:ac_view")
-
-        form = LinkForm()
-        form.request = None
-        str(form)  # renders form
-
-        # Now does not fail
-        try:
-            reverse("dcf_autocomplete:ac_view")
+            admin_reverse("link_link_autocomplete")
         except NoReverseMatch as e:
             self.fail(str(e))
