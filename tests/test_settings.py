74cd0f96710fcdc0c1b338da366171a04731d13a
Fabian Braun <fsbraun@gmx.de>
2024-06-18 10:21:39 +0200

    Update testing

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     "djangocms_frontend.contrib.card",
     "djangocms_frontend.contrib.carousel",
     "djangocms_frontend.contrib.collapse",
+    "djangocms_frontend.contrib.component",
     "djangocms_frontend.contrib.content",
     "djangocms_frontend.contrib.grid",
     "djangocms_frontend.contrib.icon",
