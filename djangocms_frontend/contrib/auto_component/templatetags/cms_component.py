931e5823edc9bf270a6914c178665810312649c8
Fabian Braun <fsbraun@gmx.de>
2025-03-23 12:30:16 +0100

    Add docsstrings for template tags

--- djangocms_frontend/contrib/auto_component/templatetags/cms_component.py
+++ djangocms_frontend/contrib/auto_component/templatetags/cms_component.py
@@ -1,19 +1,49 @@
-from django import template
+from django import forms, template
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def cms_component(context: template.Context, *args, **kwargs) -> str:
+def cms_component(context: template.Context, name, **kwargs: dict) -> str:
+    """
+    Declare a CMS frontend component in a Django template.
+
+    This template tag triggers the generation of a CMS frontend component
+    class based on the implate it is part of. The component class is generated
+    with the provided arguments as ``Meta`` properties.
+
+    Args:
+        context (template.Context): The Django template context object where
+            components are stored.
+        name: Internal name of the component. Must be unique throught a project.
+        **kwargs: Keyword arguments to be added to component's ``Meta`` class list.
+
+    Returns:
+        str: An empty string, as this function is intended for side effects only.
+    """
     if "_cms_components" in context:
-        context["_cms_components"]["cms_component"].append((args, kwargs))
+        context["_cms_components"]["cms_component"].append(([name], kwargs))
     return ""
 
 
 @register.simple_tag(takes_context=True)
-def field(context: template.Context, *args, **kwargs) -> str:
+def field(context: template.Context, field_name: str, field_type: forms.Field, **kwargs) -> str:
+    """
+    Declares a field entry for the component. It only has an effect if used in
+    conjunction with ``{% cms_component %}``.
+
+    Args:
+        context (template.Context): The template context object that may contain
+            the "_cms_components" dictionary.
+        field_name (str): The name of the field to be added to the component.
+        field_type (forms.Field): A Django form field class to be used for the field.
+        **kwargs: Arbitrary keyword arguments representing field attributes or data.
+
+    Returns:
+        str: An empty string, as this function is intended for side effects only.
+    """
     if "_cms_components" in context:
-        context["_cms_components"]["fields"].append((args, kwargs))
+        context["_cms_components"]["fields"].append(([], kwargs))
     return ""
 
 
