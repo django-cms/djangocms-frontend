4b832510f169bb03a872943c7f9adaf9013814f5
Fabian Braun <fsbraun@gmx.de>
2026-06-19 12:48:16 +0200

    Apply suggestion from @fsbraun

--- djangocms_frontend/component_base.py
+++ djangocms_frontend/component_base.py
@@ -186,7 +186,7 @@ class CMSFrontendComponent(forms.Form):
                     **(
                         {
                             "get_render_template": cls.get_render_template,
-                            "TEMPLATES": getattr(cls, "TEMPLATES", (("default", "Default),)),
+                            "TEMPLATES": getattr(cls, "TEMPLATES", (("default", "Default"),)),
                         }
                         if hasattr(cls, "get_render_template")
                         else {}
